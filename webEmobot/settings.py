eef9d725a31ff28e619696c3dc5c6cb6f70cdbc3
ajchado <70462435+ajchado@users.noreply.github.com>
2022-03-08 13:56:27 +0800

    changed email
    previous email was deactivated

--- webEmobot/settings.py
+++ webEmobot/settings.py
@@ -143,8 +143,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'zuugwe2@gmail.com'
-EMAIL_HOST_PASSWORD = 'svlgefzfdznslree'
+EMAIL_HOST_USER = 'wilmscit@gmail.com'
+EMAIL_HOST_PASSWORD = 'yfztozxihamnrqiv'
 EMAIL_USE_TLS = True
 
 # Activate Django-Heroku.
